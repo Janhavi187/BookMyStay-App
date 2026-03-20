47d06f3f5fc0db9cff9f5d09ad0289f95e1e177a
na1388 <na1388@srmist.edu.in>
2026-03-20 09:08:42 +0530

    Base project setup

---
+++ src/HotelBookingApp.java
@@ -0,0 +1,19 @@
+/**
+ * Book My Stay App
+ * This class represents the entry point of the Hotel Booking System.
+ *
+ * @author Janhavi
+ * @version 1.0
+ */
+
+public class HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to Book My Stay App ");
+        System.out.println(" Version: 1.0");
+        System.out.println("=================================");
+        System.out.println("Application started successfully!");
+    }
+}
